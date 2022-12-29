ef46b23f1b5de5147a7a87972bbe92d9e1ad7440
Oluwagbemileke Femi Oyeyoade <gbemfemalabi@gmail.com>
2022-12-29 17:22:01 +0000

    Update: Initialized solution to two number sum

---
+++ Solution/Two Number Sum/main.py
@@ -0,0 +1,29 @@
+''' Solutions to two number sum DSA problem'''
+# Array = [0,1,2,3,4,5,6]
+# Target = 3
+# output_value = [1,2]
+
+
+def two_sum_a(array, target):
+    """This is a O(n^2) time complexity 
+    but O(1) space complexity solution to the problem
+    This solution uses two loops to solve the problem"""
+    for value in array:
+        # 
+    return []
+
+
+def two_sum_b(array, target):
+    """This is a O(n) time complexity 
+    but O(n) space complexity solution to the problem
+    This solution uses hashtables to solve the problem"""
+    return []
+
+
+def two_sum_c(array, target):
+    """This is a O(nlog(n)) time complexity
+    but O(1) space complexity solution to the problem
+    This solution uses left and right indices to solve the problem"""
+    array.sort()
+    
+    return []
